b579fa7b7f24b35e1ed54332916c745142c97aa4
krzys-h <krzys_h@interia.pl>
2015-09-17 20:36:29 +0200

    Zmniejszone menu główne

--- app/src/main/assets/script.js
+++ app/src/main/assets/script.js
@@ -16,14 +16,22 @@ if ($(".button-login").length > 0)
     $(".page_infopage").hide();
 }
 
-/* Wylogowanie w tym miejscu niepotrzebne */
+/* Wylogowanie i zmiana hasła są w tym miejscu niepotrzebne, są też w rozwijanym na górze a zajmują miejsce */
 $(".col-md-3 > .list-group > .list-group-item[href='/wyloguj']").hide();
+$(".col-md-3 > .list-group > .list-group-item[href='/zmienhaslo']").hide();
+
+/* A to trzeba przerzucić bo na górze nie ma ;) */
+$(".col-md-3 > .list-group > .list-group-item[href='/report']").hide();
+$("#navbar > .navbar-right").append('<li><a href="/report">Zgłoś błąd lub sugestię</a></li>');
 
 /* Autor musi być */
-$(".col-md-3 > .list-group:nth-child(2)").append('<a class="list-group-item">Aplikacja mobilna by Krzysztof Haładyn</a>');
+$(".col-md-3 > .list-group:nth-child(2)").append('<a class="list-group-item" href="http://github.com/krzys-h/ObiadyPLOPL-Android">Aplikacja mobilna by Krzysztof Haładyn</a>');
 
 /* Dorzucamy parę rzeczy do strony zgłaszania błędów */
 if(document.location.href.indexOf("report") != -1)
 {
     $(".order-day > .list-group").append('<li class="list-group-item"></li><li class="list-group-item"><strong>Aplikacja mobilna</strong></li><li class="list-group-item"><a href="https://www.facebook.com/krzyshal">Krzysztof Haładyn</a></li>');
 }
+
+/* Muszę to dodać, muszę XD */
+$(".col-md-3 > .list-group:nth-child(2) > .list-group-item:nth-child(1)").attr("href", "https://www.youtube.com/watch?v=dQw4w9WgXcQ");
